b667f9b3b4dc891e00925609db3458475424501b
Hamish <hamish.orr@my.jcu.edu.au>
2016-08-15 11:01:45 +1000

    split into functions

--- Prac 2/asciiTable.py
+++ Prac 2/asciiTable.py
@@ -1,7 +1,8 @@
-lower = input("lower asci value:")
-upper = input("upper asci value:")
+def main():
+    get_number(int(input("input lower asci value:")), int(input("input upper asci value")))
 
-for i in range(int(lower), int(upper) + 1, 1):
- print("{:3} {:2}".format(i, chr(i)))
+def get_number(lower,upper):
+    for i in range(int(lower), int(upper) + 1, 1):
+        print("{:3} {:2}".format(i, chr(i)))
 
-
+main()
