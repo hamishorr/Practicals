bd0a4cb2170631906430a50d3d86a797afe26008
Hamish <hamish.orr@my.jcu.edu.au>
2016-08-22 10:41:06 +1000

    error controls

--- Prac 2/asciiTable.py
+++ Prac 2/asciiTable.py
@@ -1,8 +1,29 @@
 def main():
-    get_number(int(input("input lower asci value:")), int(input("input upper asci value")))
+
+    print("asci number range selector, press q to quit")
+    lower = input("input lower asci value:")
+    upper = input("input upper asci value")
+
+    finished = False
+
+    while not finished:
+        if not lower.isdecimal() or not upper.isdecimal():
+            print("enter integer values. floats and other characters are not accepted")
+
+        elif int(lower) >= 48 and int(upper) <= 58:
+            get_number(lower, upper)
+            finished = True
+
+        else:
+            print("incorrect numerical asci values, choose a number between 48 and 58")
+        lower = input("input lower asci value:")
+        upper = input("input upper asci value:")
+
+    print('thank-you for using asci finder')
 
 def get_number(lower,upper):
     for i in range(int(lower), int(upper) + 1, 1):
         print("{:3} {:2}".format(i, chr(i)))
+    return()
 
 main()
